801c000cc421d74cb32a1100356659de2c5f23d1
sud0nymm <145041893+sud0nymm@users.noreply.github.com>
2024-11-06 15:05:50 +0100

    ratingpage

---
+++ src/components/RatingPage.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './componentStyles.css'
+
+
+function RatingPage(){
+
+    return (
+        <div> <p> Rating Page </p> </div>
+    )
+}
+
+export default RatingPage
