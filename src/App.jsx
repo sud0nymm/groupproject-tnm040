a5d3083883dcc99dbf7849fc860eda4fabb0554b
sud0nymm <145041893+sud0nymm@users.noreply.github.com>
2024-11-06 15:02:48 +0100

    UpdatesAndStructure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import './components/FreePlay.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
+import {DraggableCore} from 'react-draggable'; // <DraggableCore>
+
 
 function App() {
-
   return (
-    <p> hi </p>
-  )
+    
+    <p> Start Menu Page </p>
 
+  );
 }
 
 export default App
---
+++ src/components/TakeOrder.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
+import {DraggableCore} from 'react-draggable'; // <DraggableCore>
+import './componentStyles.css'
+
+
+function TakeOrder(){
+
+    return (
+        <div> <p> Free Play Page</p> </div>
+    )
+}
+
+function DraggableBox(){
+
+    return (
+
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Draggable>
+          <div className= "testbox">
+            <p>Drag me around!</p>
+          </div>
+        </Draggable>
+      </div>
+    )
+}
+
+export default TakeOrder
