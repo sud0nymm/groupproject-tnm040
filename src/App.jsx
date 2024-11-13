5ea17695e699a6afcf66287e7b2b6f9a089085b8
Coukie5 <felixfalebrand@gmail.com>
2024-11-13 13:29:40 +0100

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -36,40 +36,14 @@ function App() {
 function StartPage() {
 
   return (
-<<<<<<< Updated upstream
     <div>   
       <div className='container'>
         <div className='sliding-background'> 
 
         </div>
       </div>
-      
-      <Link to="/freeplay">Free Play</Link> { /*make pretty pretty please*/ }
-      <br/>
-      <Link to="/takeorder">Take Play</Link>
-
-      <div className="mugg">
-        <button className="button-play"></button>
-      </div>
+      <Link to="/takeorder" className="button-play"></Link>
     </div>
-=======
-    <>
-      
-
-      <Link to="/freeplay" className="container">Free Play</Link> { /*make pretty pretty please*/}
-
-      
-      { /*Knappen för "take order", välj om det ska vara playknappen, texten eller båda som ska vara klickbara*/}
-
-
-          <Link to="/takeorder" className="button-play"></Link>
-
-
-      { /*Ta bort knapp och text och ha endast drick*/}
-    </>
-
-
->>>>>>> Stashed changes
   )
 
 }
