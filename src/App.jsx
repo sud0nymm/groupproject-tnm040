e5c8117b3e7deee36dc1b775a85226fda4e58f61
Julia041119 <144617877+Julia041119@users.noreply.github.com>
2024-11-13 13:19:54 +0100

    rgter

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,40 @@ import './App.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Draggable from 'react-draggable';
-import {DraggableCore} from 'react-draggable';
+import { DraggableCore } from 'react-draggable';
 import TakeOrder from './components/TakeOrder.jsx';
 import FreePlay from './components/FreePlay.jsx';
 import RatingPage from './components/RatingPage.jsx';
-import { BrowserRouter, Routes, Route, useParams , Link} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useParams, Link } from 'react-router-dom';
 
 
 function App() {
   return (
-    
-    
+
+
     <BrowserRouter>
-          
-        <Routes>
-          
-          <Route path="" element={ <StartPage /> }  /> {/* START PAGE */}
-        
-          <Route path="/freeplay" element={<FreePlay />} />  {/* FREE PLAY PAGE */}
 
-          <Route path="/takeorder" element={<TakeOrder />} />  {/* TAKE ORDER PAGE */}
+      <Routes>
 
-          <Route path="/ratingpage" element={<RatingPage />} />  {/* RATING PAGE */}
+        <Route path="" element={<StartPage />} /> {/* START PAGE */}
 
-        </Routes>
-      
+        <Route path="/freeplay" element={<FreePlay />} />  {/* FREE PLAY PAGE */}
+
+        <Route path="/takeorder" element={<TakeOrder />} />  {/* TAKE ORDER PAGE */}
+
+        <Route path="/ratingpage" element={<RatingPage />} />  {/* RATING PAGE */}
+
+      </Routes>
+
     </BrowserRouter>
 
   );
 }
 
-function StartPage(){
+function StartPage() {
 
   return (
+<<<<<<< Updated upstream
     <div>   
       <div className='container'>
         <div className='sliding-background'> 
@@ -51,6 +52,24 @@ function StartPage(){
         <button className="button-play"></button>
       </div>
     </div>
+=======
+    <>
+      
+
+      <Link to="/freeplay" className="container">Free Play</Link> { /*make pretty pretty please*/}
+
+      
+      { /*Knappen för "take order", välj om det ska vara playknappen, texten eller båda som ska vara klickbara*/}
+
+
+          <Link to="/takeorder" className="button-play"></Link>
+
+
+      { /*Ta bort knapp och text och ha endast drick*/}
+    </>
+
+
+>>>>>>> Stashed changes
   )
 
 }
